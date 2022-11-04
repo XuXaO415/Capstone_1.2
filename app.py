797e0610f5b2f21e8019bcc57a0c19e011e6e510
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-11-03 17:28:52 -0700

    Removed import

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import os
-import re
-
 ##############################################################################
 
 # from flask import *
@@ -36,7 +34,8 @@ app = Flask(__name__)
 
 CURR_USER_KEY = "curr_user"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL', 'postgresql:///nea_db'))
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ.get('DATABASE_URL', 'postgresql:///nea_db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
@@ -67,6 +66,7 @@ def add_user_to_g():
 # User login
 ##############################################################################
 
+
 def do_login(user):
     """Log in user."""
 
@@ -75,97 +75,100 @@ def do_login(user):
 ##############################################################################
 # User logout
 ##############################################################################
+
+
 @app.route('/logout')
 def do_logout():
     """Logout user."""
 
     if CURR_USER_KEY in session:
         del session[CURR_USER_KEY]
-        
+
         return redirect(url_for('login'))
 
 ##############################################################################
 # Homepage
 ##############################################################################
+
+
 @app.route("/", methods=["GET", "POST"])
 def homepage():
     """Homepage"""
     API_SECRET_KEY = os.getenv('API_SECRET_KEY')
-    
-    
+
     res = requests.get(
         f"https://newsapi.org/v2/everything?q=latin-america&q=asia&q=europe&language=en&sortBy=popularity&pageSize=5&domains=apnews.com,reuters.com,npr.org,bbc.com,economist.com,wsj.com,politifact.com,thebureauinvestigates.com&apiKey={API_SECRET_KEY}")
     # pdb.set_trace()
     world_new = save_article(res)
-    
-    
+
     res = requests.get(
         f"https://newsapi.org/v2/everything?q=top-news&language=en&sortBy=publishedAt&pageSize=5&domains=apnews.com,reuters.com,npr.org,economist.com,wsj.com,bbc.com,politifact.com,thebureauinvestigates.com&apiKey={API_SECRET_KEY}")
 
     latest_article = save_article(res)
-    
+
     return render_template("index.html", world_news=world_new, latest_articles=latest_article)
 
 ##############################################################################
 # User signup
 ##############################################################################
 
+
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     """Handle user registration"""
-    
+
     if CURR_USER_KEY in session:
         del session[CURR_USER_KEY]
-    
+
     form = UserAddForm()
-    
+
     if form.validate_on_submit():
         try:
             user = User.signup(
-                first_name = form.username.data,
-                last_name = form.last_name.data,
-                email = form.email.data,
-                username = form.username.data,
-                pwd = form.password.data
+                first_name=form.username.data,
+                last_name=form.last_name.data,
+                email=form.email.data,
+                username=form.username.data,
+                pwd=form.password.data
             )
             db.session.add(user)
             db.session.commit()
             get_flashed_messages()
             flash("Successfully signed up!")
-            
+
         except IntegrityError:
             flash("Sorry, that username is already taken", "error")
             return render_template("users/signup.html", form=form)
-        
+
         do_login(user)
         flash("Successfully signed up!", "success")
         return redirect('/')
-    
+
     else:
         return render_template('/users/signup.html', form=form)
 ##############################################################################
 # Login form
 ##############################################################################
-        
+
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Handle user login"""
-        
+
     form = LoginForm()
-    
+
     if form.validate_on_submit():
         user = User.authenticate(form.username.data,
                                  form.password.data)
-        
+
         if user:
             do_login(user)
             flash(f"Welcome back, {user.username}!", "success")
             return redirect("/")
-        
-        flash("Invalid credentials.", "danger")
-        
-    return render_template('users/login.html', form=form)
 
+        flash("Invalid credentials.", "danger")
+
+    return render_template('users/login.html', form=form)
 
 
 ##############################################################################
@@ -175,7 +178,7 @@ def login():
 def user_profile(user_id):
 
     user = User.query.get_or_404(user_id)
-    
+
     return render_template("users/profile.html", user=user)
 
 
@@ -185,57 +188,58 @@ def user_profile(user_id):
 @app.route("/users/<int:user_id>/edit", methods=["GET", "POST"])
 def edit_user(user_id):
     """Edit user profile"""
-    
+
     if not g.user:
         flash("Access unauthorized.", "danger")
         return redirect("/")
-    
+
     user = g.user
     form = UserEditForm(obj=g.user)
 
-        
     if form.validate_on_submit():
         if User.authenticate(form.username.data, form.password.data):
 
             user.username = form.username.data
             user.password = form.password.data
-        
+
             # db.session.add(g.user)
-            db.session.commit()   
-           
-            
+            db.session.commit()
+
             flash("Your profile has been updated.", "success")
             return redirect(f"/users/{g.user.id}")
-        else: 
+        else:
             flash("Incorrect password", "danger")
             return redirect(f"/users/{g.user.id}")
     else:
         return render_template("users/edit.html", form=form, user_id=user_id)
-        
+
 ##############################################################################
 # Delete User and remove user from database
 ##############################################################################
+
+
 @app.route("/users/delete", methods=["POST"])
 def delete_user():
     """Delete user"""
-    
+
     if not g.user:
         flash("Access unauthorized.", "danger")
         return redirect("/")
 
-    
     do_logout()
     get_flashed_messages()
     flash("Account deleted. We hope to see you again!", "success")
     # pdb.set_trace()
     db.session.delete(g.user)
     db.session.commit()
-    
+
     return redirect("/signup")
-    
+
 ##############################################################################
 # User Favorite
 ##############################################################################
+
+
 @app.route("/users/favorites/<int:likes_id>", methods=["GET", "POST"])
 def add_likes(likes_id):
     """Enables a user to like an article"""
@@ -247,17 +251,19 @@ def add_likes(likes_id):
     if Likes.query.filter_by(user_id=g.user.id, article_id=likes_id).first():
         flash("You have already liked this article", "danger")
         return redirect("/")
-    
+
     else:
         add_like = Likes(user_id=g.user.id, article_id=likes_id)
         db.session.add(add_like)
         db.session.commit()
         flash("You have successfully liked this article", "success")
         return redirect("/users/favorites/")
-    
+
 ##############################################################################
 # User Favorites
 ##############################################################################
+
+
 @app.route("/users/favorites/", methods=["GET", "POST"])
 def user_favorite():
     """Shows a list of user's liked articles"""
@@ -265,15 +271,15 @@ def user_favorite():
     articles = Likes.query.with_entities(Likes.article_id, Likes.user_id, Likes.id, Likes.timestamp,
                                          Likes.timestamp, Article.content, Article.title, Article.urlToImage, Article.description, Article.publishedAt, Article.author, Article.url,
                                          Article.date_added).filter_by(user_id=g.user.id).distinct().join(Article).order_by(Likes.id.desc()).all()
-        
+
     likes = []
-        
+
     for article in articles:
         like = Article.query.filter_by(id=article.article_id).first()
         likes.append(like)
-        
+
     return render_template("/users/favorite.html", articles=articles)
-            
+
 
 ##############################################################################
 # Delete favorite story
@@ -281,11 +287,11 @@ def user_favorite():
 @app.route("/users/delete/<int:likes_id>", methods=["GET", "POST"])
 def delete_like(likes_id):
     """Current logged in user can delete their favorite story"""
-        
+
     if not g.user:
         flash("You are not the authorized user of this account", "danger")
         return redirect("/")
-    
+
     if Likes.query.all():
         delete_like = Likes.query.filter_by(
             user_id=g.user.id, article_id=likes_id).first()
@@ -299,17 +305,21 @@ def delete_like(likes_id):
 ##############################################################################
 # Upon successful logout, redirects user to login page
 ##############################################################################
+
+
 @app.route("/logout")
 def logout():
 
     do_logout()
     flash("You have successfully logged out", "success")
-    
+
     return redirect(url_for("login"))
 
 ##############################################################################
 # Super simple 404 page
 ##############################################################################
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     """404 Not Found"""
@@ -318,6 +328,8 @@ def page_not_found(e):
 ##############################################################################
 # save_article function that saves the articles to the database
 ##############################################################################
+
+
 def save_article(res):
     """Save article to database"""
 
@@ -344,16 +356,17 @@ def save_article(res):
 ##############################################################################
 # Homepage quick link that displays latest news
 ##############################################################################
-@app.route("/latest_articles", methods =["GET", "POST"])
+
+
+@app.route("/latest_articles", methods=["GET", "POST"])
 def show_latest_articles():
     """Shows a list of latest articles from API"""
-    
- 
-    API_SECRET_KEY = os.getenv('API_SECRET_KEY')
-    
-    url = ('https://newsapi.org/v2/top-headlines?' 'language=en' 'qinTitle=query' 'apiKey={API_SECRET_KEY}')
-    res = requests.get(url)
 
+    API_SECRET_KEY = os.getenv('API_SECRET_KEY')
+
+    url = (
+        'https://newsapi.org/v2/top-headlines?' 'language=en' 'qinTitle=query' 'apiKey={API_SECRET_KEY}')
+    res = requests.get(url)
 
     res = requests.get(
         f"https://newsapi.org/v2/top-headlines?category=general&country=us&pageSize=15&apiKey={API_SECRET_KEY}")
@@ -364,24 +377,27 @@ def show_latest_articles():
 
 ##############################################################################
 
+
 @app.route("/world_news", methods=["GET", "POST"])
 def show_world_news():
     """Show world news"""
     API_SECRET_KEY = os.getenv('API_SECRET_KEY')
-    
-    url = ('https://newsapi.org/v2/top-headlines?' 'language=en' 'qinTitle=query' 'pageSize=20' 'apiKey={API_SECRET_KEY}')
+
+    url = (
+        'https://newsapi.org/v2/top-headlines?' 'language=en' 'qinTitle=query' 'pageSize=20' 'apiKey={API_SECRET_KEY}')
     res = requests.get(url)
-    
+
     res = requests.get(
         f"https://newsapi.org/v2/top-headlines?q=international&language=en&pageSize=15&apiKey={API_SECRET_KEY}")
-    
+
     # pdb.set_trace()
     world_new = save_article(res)
-    
+
     return render_template("article_list.html", articles=world_new, title='World News')
 ##############################################################################
 # Page link displays tech news
 ##############################################################################
+
 
 @app.route("/technology", methods=["GET", "POST"])
 def show_tech_news():
@@ -389,50 +405,56 @@ def show_tech_news():
     API_SECRET_KEY = os.getenv('API_SECRET_KEY')
     res = requests.get(
         f"https://newsapi.org/v2/top-headlines?category=technology&country=us&pageSize=15&apiKey={API_SECRET_KEY}")
-    
+
     tech_news = save_article(res)
-    
+
     return render_template("article_list.html", articles=tech_news, title='Technology')
-    
+
 ##############################################################################
 # Page link that displays business news
 ##############################################################################
-@app.route("/business", methods=["GET","POST"])
+
+
+@app.route("/business", methods=["GET", "POST"])
 def show_business_news():
     """Show news related to business"""
     API_SECRET_KEY = os.getenv('API_SECRET_KEY')
-    
+
     res = requests.get(
         f"https://newsapi.org/v2/top-headlines?category=business&country=us&pageSize=15&apiKey={API_SECRET_KEY}")
-    
+
     business_news = save_article(res)
- 
+
     return render_template("article_list.html", articles=business_news, title='Business')
 
 ##############################################################################
 # Page link that displays US news
 ##############################################################################
+
+
 @app.route("/us_news", methods=["GET", "POST"])
 def show_us_news():
     """Show news related to U.S"""
     API_SECRET_KEY = os.getenv('API_SECRET_KEY')
     res = requests.get(
         f"https://newsapi.org/v2/top-headlines?country=us&pageSize=15&apiKey={API_SECRET_KEY}")
-    
+
     national_news = save_article(res)
-    
+
     return render_template("article_list.html", articles=national_news, title='U.S News')
 
 ##############################################################################
 # Page link that displays science news
 ##############################################################################
+
+
 @app.route("/science", methods=["GET", "POST"])
 def show_science_news():
     """Show news related to science"""
     API_SECRET_KEY = os.getenv('API_SECRET_KEY')
     res = requests.get(
         f"https://newsapi.org/v2/top-headlines?category=science&country=us&pageSize=15&apiKey={API_SECRET_KEY}")
-    
+
     science_news = save_article(res)
 
     return render_template("article_list.html", articles=science_news, title='Science')
@@ -440,90 +462,91 @@ def show_science_news():
 ##############################################################################
 # Pag link that displays health news
 ##############################################################################
+
+
 @app.route("/health", methods=["GET", "POST"])
 def show_health_news():
     """Show news related to health"""
     API_SECRET_KEY = os.getenv('API_SECRET_KEY')
     res = requests.get(
         f"https://newsapi.org/v2/top-headlines?category=health&country=us&pageSize=15&apiKey={API_SECRET_KEY}")
-    
+
     health_news = save_article(res)
 
     return render_template("article_list.html", articles=health_news, title='Health')
 ##############################################################################
 # Search box for all news
 ##############################################################################
-@app.route("/search", methods=["GET","POST"])
+
+
+@app.route("/search", methods=["GET", "POST"])
 def search_all_articles():
     """Search for articles"""
-   
+
     HOST = os.getenv('HOST')
     API_KEY = os.getenv('API_KEY')
     API_SECRET_KEY = os.getenv('API_SECRET_KEY')
-    
+
     # pdb.set_trace()
     querystring = {
         "query": requests.args['q'], "sort_by": "relevant", "use_lucene_syntax": "true"}
-  
-    
+
     latestquery = {
         "query": requests.args['q'],  "sort_by": "recent", "use_lucene_syntax": "true"}
-    
+
     # res = requests.get(
     #     "https://api.newsapi.org/v2/everything", params=querystring, headers={"X-Api-Key": API_SECRET_KEY})
-    
-    
+
     # querytitle = {
     #     "query": "+title:\"China Is Mining Bitcoin Underground: Report\"",
     #               "sort_by": "recent", "use_lucene_syntax": "true"}
 
-    
     headers = {
         'x-rapidapi-host': HOST,
         'x-rapidapi-key': API_KEY
     }
-    
+
     url = ('https://newsapi.org/v2/everything?'
            'qinTitle=query'
            'apiKey={API_SECRET_KEY}')
 
     relevant_response = requests.get("https://api-hoaxy.p.rapidapi.com/articles", headers=headers,
-                            params=querystring)
-    
+                                     params=querystring)
+
     recent_response = requests.get("https://api-hoaxy.p.rapidapi.com/articles", headers=headers,
-                            params=latestquery)
-    
+                                   params=latestquery)
+
     response = requests.get(url)
     print(response.json())
-    
+
     title_requests = response.json()['articles']
     print(title_requests)
-    
-    
+
     relevant_articles = relevant_response.json()
     rel_art = relevant_articles['articles'][0:10]
-    
+
     recent_articles = recent_response.json()
     rec_art = recent_articles['articles'][0:10]
 
-    
     for art in rel_art:
         res = requests.get(f"https://newsapi.org/v2/top-headlines?country=us&apiKey={API_SECRET_KEY}"
-        # res = requests.get(f"https://newsapi.org/v2/everything?qinTitle=China Is Mining Bitcoin Underground: Report&apiKey={API_SECRET_KEY}"
-                                    )
+                           # res = requests.get(f"https://newsapi.org/v2/everything?qinTitle=China Is Mining Bitcoin Underground: Report&apiKey={API_SECRET_KEY}"
+                           )
         print(res.json()['articles'][0]['description'])
         art['description'] = res.json()['articles'][0]['description']
-        
+
         for art in rec_art:
             res = requests.get(f"https://newsapi.org/v2/top-headlines?country=us&apiKey={API_SECRET_KEY}"
-            # res = requests.get(f"https://newsapi.org/v2/everything?qinTitle=China Is Mining Bitcoin Underground: Report&apiKey={API_SECRET_KEY}"
-                                        )
+                               # res = requests.get(f"https://newsapi.org/v2/everything?qinTitle=China Is Mining Bitcoin Underground: Report&apiKey={API_SECRET_KEY}"
+                               )
             print(res.json()['articles'][0]['description'])
             art['description'] = res.json()['articles'][0]['description']
-            
+
     return render_template("top_articles.html", articles=rel_art, title='Search Results', articles2=rec_art)
 
 ##############################################################################
+
+
 @ app.after_request
 def add_header(req):
     """Add non-caching headers on every request."""
